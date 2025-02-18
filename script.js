5002bc1179abf26f66ae70517cf94994e6ef7e2e
AsthaKirar <asthap602@gmail.com>
2025-02-18 20:53:22 +0530

    commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1 log your name and favorite hobby to the console 
+// console.log("astha","learing english")
+
+//2 perform and log the result of 45*2-10
+// console.log(45*2-10)
+
+//3 use console.log() to display the current year
+// const newdate = new Date();
+// console.log(newdate.getFullYear());
+
+//4 crate two variable for first and last name ,concatenate and log them.
+// var first="astha";
+// var last="patel";
+
+// // console.log(first + " " + last)
+// console.log(`${first} ${last}`);
+
+
+//5 track the value of a variable by logging it before and and after updating
+// var a = 12;
+// console.log(a);
+// a=32;
+// console.log(a);
+
+//6 use console.error() to simulate an error message
+// console.error("something went wrong")
+
+
+// 7 log the square of the number 12 to the console
+// console.log(12*12)
+
+//8 print the type of a variable holding the value true
+var a = true;
+(typeof a)
